90cdbfe56c98e77f3eea59214a60a8259c2c5e8c
Maksim Sovenkov <m.sovenkov@semrush.com>
2021-09-04 19:18:41 +0300

    wdwd

--- Routes/auth/login/index.js
+++ Routes/auth/login/index.js
@@ -63,7 +63,7 @@ router.post("/", [checkUsersDBforUser, checkUserPassword], (req, res) => {
 		joinDate: res.locals.user.joinDate,
 		role: res.locals.user.role,
 	}
-	res.session.usr = existedUser
+	req.session.usr = existedUser
 	// console.log(existedUser)
 
 	res.status(200).json({ data: existedUser })
